53351286685e1c29b9c20fb10a7399dc889ed3ce
Yuan Huang <yuanhuang100@gmail.com>
2021-05-02 08:22:37 -0400

    change test file

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,18 +1,18 @@
 import unittest   # The test framework
-import bubblesort as st
+import quicksort as st
 import random
 
 class Project_Test(unittest.TestCase):
      def test_simple(self):
          testList = [4,3,2,1]
          expectedList = [1,2,3,4]
-         actualList = st.bubbleSort(testList)
+         actualList = st.quickSort(testList)
          self.assertEqual(actualList, expectedList)
          
      def test_dec(self):
-         testList = [1,2,5,3,4]
-         expectedList=[5,4,3,2,1]
-         actualList = st.bubbleSort(testList,1)
+         testList = [5, 4, 3, 2, 1]
+         expectedList=[1, 2, 3, 4, 5]
+         actualList = st.quickSort(testList)
          self.assertEqual(actualList, expectedList)
 
 
@@ -20,28 +20,28 @@ class Project_Test(unittest.TestCase):
          testList = []
          expectedList = testList.copy()
          expectedList.sort()
-         actualList = st.bubbleSort(testList)
+         actualList = st.quickSort(testList)
          self.assertEqual(actualList, expectedList)
 
      def test_alpha(self):
          testList = ['Judy','Dave','Holly','Jeff']
          expectedList = testList.copy()
          expectedList.sort()
-         actualList = st.bubbleSort(testList)
+         actualList = st.quickSort(testList)
          self.assertEqual(actualList, expectedList)
 
      def test_negative(self):
          testList = [-4,-3,-2,-1]
          expectedList = testList.copy()
          expectedList.sort()
-         actualList = st.bubbleSort(testList)
+         actualList = st.quickSort(testList)
          self.assertEqual(actualList, expectedList)
          
      def test_random(self):
          testList = [random.randint(0, 20),random.randint(0, 20),random.randint(0, 20),random.randint(0, 20)]
          expectedList = testList.copy()
          expectedList.sort()
-         actualList = st.bubbleSort(testList)
+         actualList = st.quickSort(testList)
          self.assertEqual(actualList, expectedList)
 
 if __name__ == '__main__':
